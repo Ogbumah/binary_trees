6e487ebc59846c1f7851b349929397cba8a64e88
ogbumah <ogbumalucky@gmail.com>
2022-09-23 06:52:39 +0100

    commit

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,44 +1,45 @@
-#include <stdio.h>
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - measures the balance factor of a binary tree
- * @tree: pointer to the root node of the tree
- * Return: balance factor If tree is NULL, return 0
+ * binary_tree_balance - measure the balance factor of a binary tree
+ * @tree: pointer to root node of tree to measure
+ * Return: balance factor left height minus right height, or 0 if tree is NULL
  */
-
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left, right, count;
+	int left, right;
 
 	if (tree == NULL)
 		return (0);
-	if (tree->left == NULL && tree->right == NULL)
-		return (0);
-	left = aux_tree_height(tree->left);
-	right = aux_tree_height(tree->right);
 
-	count = left - right;
-	return (count);
+	left = custom_binary_tree_height(tree->left);
+	right = custom_binary_tree_height(tree->right);
+
+	return (left - right);
 }
 
 /**
- * aux_tree_height - height of binary tree
- * @tree: pointer to the root node of the tree
- * Return: height
+ * custom_binary_tree_height - Measure the height
+ * of a binary tree from a given node
+ * @tree: pointer to node of tree to measure
+ * Description: Edited to work with balance factor function
+ * Return: height of tree or -1 if NULL
  */
-
-size_t aux_tree_height(const binary_tree_t *tree)
+int custom_binary_tree_height(const binary_tree_t *tree)
 {
-	int height_left, height_right;
+	int left, right;
 
 	if (tree == NULL)
-		return (0);
-	height_left = aux_tree_height(tree->left) + 1;
-	height_right = aux_tree_height(tree->right) + 1;
+		return (-1);
 
-	if (height_left > height_right)
-		return (height_left);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
+	left = custom_binary_tree_height(tree->left) + 1;
+	right = custom_binary_tree_height(tree->right) + 1;
+
+	if (left > right)
+		return (left);
 	else
-		return (height_right);
+		return (right);
 }
